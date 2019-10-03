a07f8ebe7472a3041a1c428e36751e179d6d6ad4
Gabriel Beckers <gabriel@gbeckers.nl>
2019-10-03 12:40:01 +0200

    update license to match the original

--- jadeR.py
+++ jadeR.py
@@ -1,25 +1,4 @@
 #!/usr/bin/env python
-#######################################################################
-# jadeR.py -- Blind source separation of real signals
-#
-# Version 1.9
-#
-# Copyright 2005, Jean-Francois Cardoso (Original MATLAB code)
-# Copyright 2007-2019, Gabriel J.L. Beckers (NumPy translation)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################
 
 # This file can be either used from the command line (type
 # 'python jadeR.py --help' for usage, or see docstring of function main 
